28e7eb6f2cb878e41f7ee67c3ac00c486cd71c99
peperoncino <2wua4nlyi@gmail.com>
2021-11-29 00:40:32 +0900

    run on docker

--- app/db.py
+++ app/db.py
@@ -2,11 +2,6 @@ import os
 
 import psycopg2
 from psycopg2.extras import DictCursor
-from dotenv import load_dotenv
-
-
-if os.environ.get("ENVIRONMENT") is None:
-    load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
--- app/slackbot_settings.py
+++ app/slackbot_settings.py
@@ -1,13 +1,6 @@
 import functools
 import os
 
-from dotenv import load_dotenv
-
-import db
-
-if os.environ.get("ENVIRONMENT") is None:
-    load_dotenv()
-
 API_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
 PLUGINS = ["response"]
